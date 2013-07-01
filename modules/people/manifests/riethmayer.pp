a0853373e1b7bfcac5f9969380f9a1389b003d00
Jan Riethmayer <jan@riethmayer.de>
2013-07-01 19:59:43 +0200

    setup whole machine with all binaries like browsers.

--- modules/people/manifests/riethmayer.pp
+++ modules/people/manifests/riethmayer.pp
@@ -2,7 +2,23 @@ class people::riethmayer {
   include emacs
   include tmux
   include iterm2::stable
+  include erlang
+  include heroku
+  include java
+  include skitch
+  include dropbox
+  include screenhero
+  include virtualbox
+  include chrome
+  include firefox
+  include googledrive
+  include graphviz
+  include imagemagick
+  include onepassword
+  include propane
+  include skype
 
+  
   $home     = "/Users/${::luser}"
   $my       = "${home}/src"
   $dotfiles = "${my}/dotfiles"
@@ -11,4 +27,13 @@ class people::riethmayer {
     source  => 'riethmayer/dotfiles',
     require => File[$my]
   }
+
+  include vagrant
+  vagrant::plugin { 'omnibus': }
+  vagrant::plugin { 'librarian-chef': }
+  
+  include postgresql
+  # to create a db:
+  # postgresql::db { 'mydb': } 
+
 }
