740f83fb7e19fae4c49a7a8fa65baa30ad1246e6
Jan Riethmayer <jan@riethmayer.de>
2013-08-03 00:22:02 +0200

    add gitconfig within puppet.

--- modules/people/manifests/riethmayer.pp
+++ modules/people/manifests/riethmayer.pp
@@ -22,29 +22,63 @@ class people::riethmayer {
   $my       = "${home}/src"
   $dotfiles = "${my}/dotfiles"
 
+  git::config::global { 'user.name':
+    value => 'Jan Riethmayer'
+  }
+
+  git::config::global { 'user.email':
+    value => 'jan@riethmayer.de'
+  }
+
+  git::config::global { 'alias.st':
+    value => 'status'
+  }
+  git::config::global { 'alias.ci':
+    value => 'commit'
+  }
+
+  git::config::global { 'alias.br':
+    value => 'branch'
+  }
+
+  git::config::global { 'alias.co':
+    value => 'checkout'
+  }
+
+  git::config::global { 'alias.df':
+    value => 'diff'
+  }
+
+  git::config::global { 'alias.lg':
+    value => 'log -p'
+  }
+
+  git::config::global { 'alias.who':
+    value => 'shortlog -s --'
+  }
+
+  git::config::global { 'push.default':
+    value => 'curent'
+  }
+
   repository { $dotfiles:
     source  => 'riethmayer/dotfiles',
     require => File[$my]
   }
 
-  file { "${home}/.gitconfig":
-       ensure => 'link',
-       target => "${dotfiles}/gitconfig",
-  }
-
   file { "${home}/.tmux.conf":
-       ensure => 'link',
-       target => "${dotfiles}/tmux.conf",
+    ensure => 'link',
+    target => "${dotfiles}/tmux.conf",
   }
 
   file { "${home}/.profile":
-       ensure => 'link',
-       target => "${dotfiles}/bashrc",
+    ensure => 'link',
+    target => "${dotfiles}/bashrc",
   }
 
   file { "${home}/bin":
-       ensure => 'link',
-       target => "${dotfiles}/bin",
+    ensure => 'link',
+    target => "${dotfiles}/bin",
   }
 
   repository { "${home}/.emacs.d":
